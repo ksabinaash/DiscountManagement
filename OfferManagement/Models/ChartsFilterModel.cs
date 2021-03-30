e7373f250dbf59d59a3a8625e3bf8bfa44e859a7
sampath.rajaraman <sampath6.sbkv@gmail.com>
2021-03-30 11:04:37 +0530

    Chart Filter Fix

--- OfferManagement/Models/ChartsFilterModel.cs
+++ OfferManagement/Models/ChartsFilterModel.cs
@@ -9,10 +9,10 @@ namespace OfferManagement.Models
     public class ChartsFilterModel
     {
         [Display(Name = "From Name :")]
-        public DateTime FromDate { get; set; } = DateTime.Now.AddDays(Convert.ToInt32(System.Configuration.ConfigurationManager.AppSettings["ChartsDaysRange"]));
+        public DateTime FromDate { get; set; } = DateTime.UtcNow.AddDays(Convert.ToInt32(System.Configuration.ConfigurationManager.AppSettings["ChartsDaysRange"]));
 
         [Display(Name = "To Date :")]
-        public DateTime ToDate { get; set; } = DateTime.Now;
+        public DateTime ToDate { get; set; } = DateTime.UtcNow;
 
         [Required]
         [Display(Name = "Lab Name* :")]
